2272a61a465dd68532f6fa02f6fb8072064356e0
omar214 <omarmohamed2322000@gmail.com>
2023-02-06 23:38:34 +0200

    edit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function countDown() {
 	let today = new Date();
-	let vDate = new Date("2023-7-21 10:00:00");
+	let vDate = new Date('2023-7-21 10:00:00');
 	const t = vDate.getTime() - today.getTime();
 
 	const day = Math.floor(t / (1000 * 60 * 60 * 24));
@@ -20,16 +20,3 @@ function countDown() {
 }
 
 setInterval(countDown, 1000);
-
-const audio = new Audio('assets/taka.mp3');
-audio.loop = true;
-
-function toggle_taka() {
-	if (audio.paused) {
-		audio.play()
-		document.getElementById('taka_player').innerText = 'pause'
-	} else {
-		audio.pause()
-		document.getElementById('taka_player').innerText = 'play'
-	}
-}
